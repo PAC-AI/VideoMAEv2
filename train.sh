f4b06588c3697de2583dda3261ca4c9ba07bfa3b
Shrinidhi KL <klshrinidhi@gmail.com>
2024-01-25 07:07:46 +0000

    Change num nodes and IP.

--- train.sh
+++ train.sh
@@ -4,9 +4,9 @@ set -o xtrace
 
 torchrun \
 	--nproc_per_node 8 \
-	--nnodes 3 \
+	--nnodes 4 \
 	--node_rank 0 \
 	--rdzv_id 4532 \
 	--rdzv_backend c10d \
-	--rdzv_endpoint 10.128.0.11:4532 \
+	--rdzv_endpoint 10.128.15.246:4532 \
 	run_mae_pretraining.py
