4176fa81113511d11d7fa120891318b302cff548
Shrinidhi KL <klshrinidhi@gmail.com>
2024-01-25 22:18:17 +0000

    Read every clip to make sure it is readable.

--- write_clip_list.py
+++ write_clip_list.py
@@ -3,39 +3,86 @@ import sys
 import pathlib
 
 from tqdm import tqdm
-import numpy as np
-from skvideo.io import FFmpegReader
+# import numpy as np
+# from skvideo.io import FFmpegReader
 from decord import VideoReader,cpu
+import joblib
 
 
-def is_clip_readable(clip_f):
+def get_n_frames(clip_f):
     try:
-        # reader = VideoReader(str(clip_f))
-        # n_frames = sum(1 for f in reader)
-        reader = FFmpegReader(str(clip_f))
-        n_frames = sum(1 for f in reader.nextFrame())
-        reader.close()
-        assert n_frames > 32
+        reader = VideoReader(str(clip_f))
+        n_frames = sum(1 for f in reader)
+        # reader = FFmpegReader(str(clip_f))
+        # n_frames = sum(1 for f in reader.nextFrame())
+        # reader.close()
     except KeyboardInterrupt:
         sys.exit(1)
     except:
-        return False
+        return 0
     else:
-        return True
+        return n_frames
 
-corrupt = 0
+
 data_d = pathlib.Path('/data/icu')
-device_d = data_d / sys.argv[1]
-clip_fs = [f for f in device_d.iterdir()]
-clip_list_f = pathlib.Path('/data/clips_' + sys.argv[1].replace('/','_') + '.txt')
+clip_list_f = pathlib.Path('/data/clips.txt')
+n_frames_f = pathlib.Path('/data/n_frames.txt')
+corrupt_list_f = pathlib.Path('/data/corrupt_clips.txt')
+
+clip_fs = [f
+           for pat_d in data_d.iterdir()
+           for device_d in pat_d.iterdir()
+           for f in device_d.iterdir()
+           if f.suffix == '.mp4']
+pool = joblib.Parallel(n_jobs=os.cpu_count())
+n_frames = list()
+jobs = list()
+for clip_f in tqdm(clip_fs,desc='clip'):
+    jobs.append(joblib.delayed(get_n_frames)(clip_f))
+    if len(jobs) == os.cpu_count()*10:
+        n_frames.extend(pool(jobs))
+        jobs = list()
+n_frames.extend(pool(jobs))
+assert len(clip_fs) == len(n_frames)
 with open(clip_list_f,'w') as clip_list_f:
-    for clip_f in tqdm(clip_fs,sys.argv[1]):
-        if clip_f.suffix != '.mp4':
+    for f,n in zip(clip_fs,n_frames):
+        if n == 0:
             continue
-        if is_clip_readable(clip_f):
-            clip_list_f.write(f'{clip_f.relative_to(data_d)} 0 -1'+'\n')
-            clip_list_f.flush()
-        else:
-            corrupt += 1
-            print(f'corrupt: {clip_f}')
-print(f'num corrupt clips: {corrupt}')
+        clip_list_f.write(f'{f.relative_to(data_d)} 0 -1'+'\n')
+with open(n_frames_f,'w') as n_frames_f:
+    for f,n in zip(clip_fs,n_frames):
+        if n == 0:
+            continue
+        n_frames_f.write(f'{f.relative_to(data_d)} {n}'+'\n')
+with open(corrupt_list_f,'w') as corrupt_list_f:
+    for f,n in zip(clip_fs,n_frames):
+        if n == 0:
+            corrupt_list_f.write(f'{f.relative_to(data_d)}'+'\n')
+print('num frames total:',sum(n_frames))
+
+# n_frames_total = 0
+# n_corrupt = 0
+# clip_fs = [f
+#            for pat_d in data_d.iterdir()
+#            for device_d in pat_d.iterdir()
+#            for f in device_d.iterdir()
+#            if f.suffix == '.mp4']
+# clip_list_f = open(clip_list_f,'a') 
+# n_frames_f = open(n_frames_f,'a')
+# corrupt_list_f = open(corrupt_list_f,'a')
+# pbar = tqdm(clip_fs,desc='clip')
+# for clip_f in pbar:
+#     n_frames = get_n_frames(clip_f)
+#     if n_frames == 0:
+#         n_corrupt += 1
+#         corrupt_list_f.write(str(clip_f.relative_to(data_d))+'\n')
+#         pbar.set_postfix({'n_corrupt':n_corrupt})
+#         continue
+#     n_frames_total += n_frames
+#     clip_list_f.write(f'{clip_f.relative_to(data_d)} 0 -1'+'\n')
+#     n_frames_f.write(f'{clip_f.relative_to(data_d)} {n_frames}'+'\n')
+#     pbar.set_postfix({'n_corrupt':n_corrupt,
+#                       'n_frames':n_frames_total})
+# clip_list_f.close()
+# n_frames_f.close()
+# corrupt_list_f.close()
