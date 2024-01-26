4857b3826dc2be5fdb0364c2617b3a11d853588d
Shrinidhi KL <klshrinidhi@gmail.com>
2024-01-26 21:27:31 +0000

    Sync ckpts for node-rank not 0.

--- train.sh
+++ train.sh
@@ -4,15 +4,18 @@ set -o nounset
 set -o errexit
 set -o xtrace
 
+NODE_RANK=0
 RANK0_IP='10.128.15.246'
 RANK0_PORT=4532
 
-rsync -az ${RANK0_IP}:/data/output/ /data/output
+if [ ${NODE_RANK} -neq 0 ]; then
+	rsync -az ${RANK0_IP}:/data/output/ /data/output
+fi
 
 torchrun \
 	--nproc_per_node 8 \
 	--nnodes 4 \
-	--node_rank 0 \
+	--node_rank ${NODE_RANK} \
 	--rdzv_id ${RANK0_PORT} \
 	--rdzv_backend c10d \
 	--rdzv_endpoint ${RANK0_IP}:${RANK0_PORT} \
