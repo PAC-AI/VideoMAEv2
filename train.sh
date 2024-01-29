bb3554ef0f026ce3c012d0a83880402359a0ba18
Shrinidhi KL <klshrinidhi@gmail.com>
2024-01-29 20:31:24 +0000

    Set env var for omp threads.

--- train.sh
+++ train.sh
@@ -5,6 +5,7 @@ set -o errexit
 set -o xtrace
 
 NODE_RANK=0
+NPROC_PER_NODE=8
 RANK0_IP='10.128.15.246'
 RANK0_PORT=4532
 
@@ -12,8 +13,9 @@ if [ ${NODE_RANK} != 0 ]; then
 	rsync -az ${RANK0_IP}:/data/output/ /data/output/
 fi
 
+export OMP_NUM_THREADS=$(($(nproc)/${NPROC_PER_NODE}))
 torchrun \
-	--nproc_per_node 8 \
+	--nproc_per_node ${NPROC_PER_NODE} \
 	--nnodes 4 \
 	--node_rank ${NODE_RANK} \
 	--rdzv_id ${RANK0_PORT} \
