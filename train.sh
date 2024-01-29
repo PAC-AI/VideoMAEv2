247105dbabf77b44abba8e84addef0c44b49b90d
Shrinidhi KL <klshrinidhi@gmail.com>
2024-01-29 22:04:26 +0000

    Change num nodes to 5.

--- train.sh
+++ train.sh
@@ -5,18 +5,19 @@ set -o errexit
 set -o xtrace
 
 NODE_RANK=0
+NNODES=5
 NPROC_PER_NODE=8
 RANK0_IP='10.128.15.246'
 RANK0_PORT=4532
 
 if [ ${NODE_RANK} != 0 ]; then
-	rsync -az ${RANK0_IP}:/data/output/ /data/output/
+	rsync -azP ${RANK0_IP}:/data/output/ /data/output/
 fi
 
 export OMP_NUM_THREADS=$(($(nproc)/${NPROC_PER_NODE}))
 torchrun \
 	--nproc_per_node ${NPROC_PER_NODE} \
-	--nnodes 4 \
+	--nnodes ${NNODES} \
 	--node_rank ${NODE_RANK} \
 	--rdzv_id ${RANK0_PORT} \
 	--rdzv_backend c10d \
